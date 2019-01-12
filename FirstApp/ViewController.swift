8ad10c9906d17c59553f2f01f321da6ead7fdea8
Alevtina Haponova <alevtina@MacBook-Pro-alevtina.local>
2019-01-12 13:46:44 +0200

    initial commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Alevtina Haponova on 1/12/19.
+//  Copyright © 2019 Alevtina Haponova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
