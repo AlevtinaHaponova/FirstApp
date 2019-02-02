12987ba767ddc928944d03e120cedb929370c4af
Alevtina Haponova <alevtina@MacBook-Pro-alevtina.local>
2019-02-02 20:01:28 +0200

    show name Label

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var buttonHello: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        buttonHello.layer.cornerRadius = buttonHello.bounds.width / 5
@@ -18,6 +21,15 @@ class ViewController: UIViewController {
         buttonHello.clipsToBounds = true
     }
 
-
+    @IBAction func changeNameAction(_ sender: Any) {
+        
+//        if nameLabel.text == "Alina" {
+//            nameLabel.text = "Molodec"
+//        } else {
+//            nameLabel.text = "Alina"
+//        }
+        nameLabel.text = nameLabel.text == "Alina" ?  "Molodec" : "Alina"
+    } 
+    
 }
 
